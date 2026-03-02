4ee437584df7f8282ba035eadcc623b98089053e
aliyss <silyas.bieri@lagomio.com>
2026-03-02 06:13:25 +0100

    fastify host

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 const FASTIFY_PORT = Number(process.env.PORT) || 10000;
 
-app.listen({ port: FASTIFY_PORT });
+app.listen({ port: FASTIFY_PORT, host: "0.0.0.0" });
 
 console.log(
   `🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`,
